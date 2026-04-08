d3fd6fc5006e142af5f6eef888ba06124d3ab8df
Matthew van Eerde <mvaneerde@gmail.com>
2026-04-08 15:52:43 -0700

    Add tests for triggering usage prompt

--- src/tests/test_main.cpp
+++ src/tests/test_main.cpp
@@ -1,9 +1,4 @@
-#include <gtest/gtest.h>
-#include "common.h"
-
-TEST(Basic, TrivialTest) {
-    EXPECT_EQ(2 + 2, 4);
-}
+#include "test_common.h"
 
 int wmain(int argc, LPCWSTR argv[]) {
     wchar_t **v = const_cast<wchar_t **>(argv);
