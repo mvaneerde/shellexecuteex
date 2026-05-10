cc5a2d1a081fa9e2f13787cfaf591f3dfec909c8
Matthew van Eerde <mvaneerde@gmail.com>
2026-05-10 10:37:37 -0700

    Add CoInitializeEx/CoUninitialize mock and tests

--- src/windows-api.cpp
+++ src/windows-api.cpp
@@ -1,7 +1,15 @@
 #include "common.h"
 
+HRESULT WindowsApi::CoInitializeEx(LPVOID reserved, DWORD coinit) {
+    return ::CoInitializeEx(reserved, coinit);
+}
+
 BOOL WindowsApi::CloseHandle(HANDLE h) {
-    return ::CloseHandle(h);
+   return ::CloseHandle(h);
+}
+
+void WindowsApi::CoUninitialize() {
+    return ::CoUninitialize();
 }
 
 HWND WindowsApi::GetConsoleWindow() {
